d363ebe96cad7373f7380d90fc508fb08d8ae75c
Shubham1u <123749373+Shubham1u@users.noreply.github.com>
2026-08-31 10:21:07 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,15 +1,15 @@
-a=int(input("enter a first number: "))
+a=eval(input("enter a first number: "))
 operator=input("enter an operator (+, -, *, /): ")
-b=int(input("enter a second number: "))
+b=eval(input("enter a second number: "))
 operators=input("enter an operator (+, -, *, /): ")
-c=int(input("enter a third number: "))
+c=eval(input("enter a third number: "))
 if operator=='+':
     print("result:",a+b+c)
 elif operator=='-':
-    print("result:",a-b-C)
+    print("result:",a-b-c)
 elif operator=='*':
-    print("result:",a*b*C)
+    print("result:",a*b*c)
 elif operator=='/':
-    print("result:",a/b/C)
+    print("result:",a/b/c)
 else:   
         print("Error: Division by zero is not allowed.")
