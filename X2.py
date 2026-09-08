0441e5d48cdbb72b4f83ac37fa49466357d286ef
Shubham1u <nushashubx@gmail.com>
2026-09-08 09:26:34 +0530

    Add new file X2.py with initial code structure

---
+++ X2.py
@@ -0,0 +1,195 @@
+a="apple"
+# print("i want to eat ", a)
+# name="shubham"
+# print(name[0])
+# print(name[1])
+# print(name[2])
+# print(name[3])
+# print(name[4])
+# print(name[5])
+# print(name[6])
+
+
+ 
+# print("------ex2------")
+
+# age=int(input("enter your age:"))
+# if age>=18: 
+#     print("You can drive")
+# else:
+#     print("You cannot drive")
+
+# print("------ex3------")
+
+# marks=int(input("enter of marks:"))
+# if marks>=80:
+#     print("1- you got 1st division ")
+# elif marks>=60:
+#     print("2- you got 2nd division")
+# elif marks>=40:
+#     print("3- you got 3rd division")
+# else:
+#     print("you got failed")
+
+
+
+
+# print("------ex4------")
+
+# marks=int(input("enter of marks:"))
+# if marks>=300 and marks<=500:
+#     print("1- you got 1st dvision ")
+# if marks>=250 and marks<300:
+#     print("2- you got 2nd division")
+# if marks>=200 and marks<250:
+#     print("3- you got 3rd division")
+# else:
+#     print("you got failed")
+
+
+
+
+# print("------ex5------")
+
+# marks=int(input("enter of marks:"))
+# if marks>=300 and marks<=500:
+#     print("1- you got 1st dvision ")
+# if marks>=250 and marks<300:
+#     print("2- you got 2nd division")
+# if marks>=200 and marks<250:
+#     print("3- you got 3rd division")
+# else:
+#     print("you got failed")
+
+
+
+
+# print("------ex6------")
+
+
+# c=float(input("Temperature in Celsius: {c}"))
+# print(c)
+# f=(c*9/5)+32
+# print(f"Temperature in Fahrenheit: {f}")
+# k=(c+273.15)
+# print(f"Temperature in Kelvin: {k}")
+
+# print("------ex7------")
+
+# print("enter your first number: ")
+# first=int(input())
+# print("enter your second number: ")
+# second=int(input())
+# if first>second:
+#     print("greater number is ", first)
+# elif first<second:
+#     print("greater number is ", second)
+# else:
+#     print("both numbers are equal")
+
+# i=2
+# while(i<20):
+#     j=2
+#     while (j<=(i/j)):
+#         if not(i%j):
+#             print("enter first number: ")
+#             first=float(input())
+# print("enter second number: ")
+# second=float(input())
+# if first>second:
+#     print("greater number is ", first)
+# elif second>first:
+#     print("both are equal") 
+
+
+
+
+# print("------ex8------")
+
+# fruit=["banana","apple", "mango"]
+# for index in range (len(fruit)):
+#     print("i want to eat ", fruit[index])
+#     print("GOOD MORNING")
+
+
+# note=10
+# note_value=500
+# total_value=note*note_value
+# print("Rani has %d note of %d rupeees, so the money she has is %d" %(note,note_value,total_value))
+
+
+
+
+# print("------ex9------")
+
+# i=2
+# while(i<20):
+#     j=2
+#     while (j<=(i/j)):
+#         if not (i%j):
+#             break
+#         j+=1
+#     else:
+#         print(i)
+#     i+=1
+
+# print("enter first number: ")
+# first=float(input())
+# print("enter second number: ")
+# second=float(input())
+# if first>second:
+#     print("greater number is ", first)
+# elif second>first:
+#     print("greater number is ", second)
+# else:
+#     print("both are equal") 
+
+        
+
+# print("------ex10------")
+
+# for i in range(5, 0, -1):      #for 
+#     print(i)
+#     # i>=1
+#     # i=i-1
+
+
+# print('shubh1')
+
+# for i in range(1, 6):      #for 
+#     print(i)
+    
+# print('........')
+
+
+# i=5
+# while i>=1:
+#     print(i)
+#     i=i-1
+# print('.........')
+# i=1
+# while i<=5:
+#     print(i)
+#     i=i+1
+
+# print("shubh")
+
+# i=5
+# while i>=1:
+#     print(i)
+#     i=i-1
+
+
+# print("shubh3")
+
+
+# for i in range(5, 0, -1):      #for
+#     print("*"*i)
+
+
+# print("shubh4")
+
+# i=5
+# while i>=1:
+#     print("*"*i)
+#     i=i-1
