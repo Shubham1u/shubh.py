fbad50a0040aa6055a896d5ea0b767be62bd7669
Shubham1u <nushashubx@gmail.com>
2026-09-10 14:21:40 +0530

    Added new file

---
+++ loop ctr st.py
@@ -0,0 +1,15 @@
+# #break state
+# var=0
+# while var>=0:
+#     print("current variable:-",var)
+#     var=var+1
+#     if var==5:
+#         break
+#         print("be happy")
+
+
+#continue statement
+for letter in 'Python':
+    if letter=='h':
+        continue
+    print('Current Letter:-',letter)
