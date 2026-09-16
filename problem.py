b3436d1462f12999937c9fda2d25477cdcccb3ce
Shubham1u <nushashubx@gmail.com>
2026-09-16 10:18:50 +0530

    added new file

--- problem.py
+++ problem.py
@@ -1,7 +1,62 @@
-a=input("whats your problem my bhakt? dukh/pareshani/chinta:-")
-if a=="dukh":
-    print("life me lag gaye lode")
-elif a=="pareshani":
-    print("rangam ki gend marlo pareshani khatm ho jaayegi")
-elif a=="chinta":
-    print("tension mat le tu marane wala hai")
+# # QUESTION 1
+
+# user_name = str(input("enter your name :"))
+# district = str(input("enter your district name :"))
+# age = int(input("enter your age :"))
+
+# if district == "jaipur":
+#     if age >= 16:
+#         print("you are eligible for driving licence")
+#     else:
+#         print("you are not eligible for driving licence")
+# else:
+#     if age >= 18:
+#         print("you are eligible for driving licence")
+#     else:
+#         print("you are not eligible for driving licence")
+
+# print("------------------------------------------------------------------------------")
+
+# #QUESTION 2
+
+# s = int(input("enter number : "))
+# for i in range(1, s + 1):
+#     print(" " * (s - i) + "* " * i) 
+
+# print("--------------------------------------------------------------------------------")
+
+# # QUESTION 3
+
+n = int(input("enter  numbers :"))
+a = 0
+b = 1
+for i in range (n):
+    print(a)
+    c=a+b
+    a=b
+    b=c
+# print("----------------------------------------------------------------------------------")
+    
+# # QUESTION 4 
+
+# s = input("enter a string: ")
+# if s == s[: :-1]:
+#     print("pelindrome")
+    
+# else:
+#     print("not pelindrome")
+    
+
+# # print("QUESTION 5..................................................")
+
+
+# prime = int(input("enter a number to check if it's prime : "))
+# if prime<=1:
+#     print(prime,"is not a prime number")
+# elif prime > 1:
+#     for i in range(2, int(prime//2)+1):
+#         if (prime % i) == 0:
+#             print(prime, "is not a prime number")
+#             break
+#     else:
+#         print(prime, "is a prime number")
